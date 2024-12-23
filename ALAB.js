1d46680dcf2b66874ae08c30f86cd02049543a72
shila <shilapattewar16@gmail.com>
2024-12-23 00:14:43 -0600

    first ALAB javascript

---
+++ ALAB.js
@@ -0,0 +1,36 @@
+//Create variable declarations using both let and const.
+//Apply different types of data literals.
+const firstName = "John";
+const lastName = " Doe";
+const x = 10;
+const y = 5;
+let score = 0;
+
+//Use arithmetic operators to manipulate data.
+console.log(score = x+y);
+
+//Use comparison operators to compare data.
+console.log(x >= y ? x : y)
+
+//Perform string concatenations.
+console.log( firstName + score);
+console.log(firstName + "" + lastName);
+
+//Implement escape sequences in strings for special characters.
+// showing the name in newline using \n  
+console.log(firstName + "\n" + lastName);
+
+//Use template literals for string interpolation and multi-line strings.
+
+const fullName = `Hello ! ${firstName}`;
+console.log(fullName);
+
+
+//multi-line strings 
+
+const multiline = `hello 
+my full
+name is 
+John Doe !`;
+
+console.log(multiline);
